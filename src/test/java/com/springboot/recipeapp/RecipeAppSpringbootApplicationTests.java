887ccf1f0fd885f5d7ce55bed91ab852df14067f
vaibhavm <vaibhavmittal.26@gmail.com>
2018-08-02 01:44:44 +0530

    adding default spring test

--- src/test/java/com/springboot/recipeapp/RecipeAppSpringbootApplicationTests.java
+++ src/test/java/com/springboot/recipeapp/RecipeAppSpringbootApplicationTests.java
@@ -1,5 +1,6 @@
 package com.springboot.recipeapp;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,5 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class RecipeAppSpringbootApplicationTests {
 
-	
+	@Test
+	public void contextLoads() {
+	}
 }
